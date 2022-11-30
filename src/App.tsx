0f1469375878ae2340bca980fbe7af801931ec64
Andrey Mazepas <andrey@formant.io>
2022-11-30 19:32:07 -0300

    Installing libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { Authentication, Fleet } from '@formant/data-sdk'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { FormantColor } from './formantColor';
-import { GroundPlane } from './objects/GroundPlane';
-import { Axes } from './objects/Axes';
 import ThreeComponent from './components/ThreeComponent';
+import React from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(0)
   const [telemetry, setTelemetry] = useState(null);
   const getDevice = async () => {
     await Authentication.waitTilAuthenticated();
@@ -28,51 +22,6 @@ function App() {
     getDevice();
     console.log(document.getElementById('threejs-canvas'))
     const threeComponent = new ThreeComponent(document.getElementById('threejs-canvas'));
-
-    // const root = new THREE.Object3D();
-    // const scene = new THREE.Scene();
-    // scene.add(root);
-    // root.rotateX(-Math.PI / 2);
-
-    // scene.background = new THREE.Color(FormantColor.module);
-    // scene.add(new THREE.AxesHelper());
-
-    // const camera = new THREE.PerspectiveCamera(
-    //   50,
-    //   window.innerWidth / window.innerHeight,
-    //   1,
-    //   1000
-    // );
-    // camera.position.z = 2;
-    // camera.position.y = 2;
-    // camera.position.x = 2;
-
-    // const canvas = document.getElementById('threejs-canvas') || undefined;
-    // const renderer = new THREE.WebGL1Renderer({
-    //   canvas,
-    //   antialias: true
-    // })
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild(renderer.domElement);
-
-    // const ambientLight = new THREE.AmbientLight('#BAC4E2', 1);
-
-    // root.add(new GroundPlane());
-    // root.add(new Axes(false));
-
-
-    // scene.add(ambientLight);
-
-
-    // const controls = new OrbitControls(camera, renderer.domElement);
-
-    // const animate = () => {
-    //   renderer.render(scene, camera);
-    //   controls.update();
-    //   window.requestAnimationFrame(animate);
-    // }
-
-    // animate();
   }, [])
 
   return <></>
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import './reset.css';
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('universe-3d-viewer') as HTMLElement).render(
   <React.StrictMode>
     <App />
     <canvas id="threejs-canvas" />
