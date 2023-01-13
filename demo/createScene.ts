94c283e81a379e7c896fdfe06313eb7fad1615fa
andrey-mazepas <andrey@formant.io>
2023-01-13 20:00:38 -0300

    Adding visibility to custom module setting

--- demo/createScene.ts
+++ demo/createScene.ts
@@ -7,7 +7,8 @@ export const FARM_BOT_3_DEVICE_ID = 'farmbot3';
 
 function createSatelliteLayer(
   baseStationLocationStream: string,
-  size: number
+  size: number,
+  mapVisibility: boolean = true
 ): SceneGraphElement {
   return {
     id: uuid.v4(),
@@ -16,7 +17,7 @@ function createSatelliteLayer(
     name: 'Satellite Map',
     deviceContext: '',
     children: [],
-    visible: true,
+    visible: mapVisibility,
     position: { type: 'manual', x: 0, y: 0, z: 0 },
     fieldValues: {
       size: {
@@ -39,7 +40,8 @@ function createSatelliteLayer(
 function createFarmbot(
   name: string,
   deviceId: string,
-  odometryStream: string
+  odometryStream: string,
+  deviceVisibility: boolean = true
 ): SceneGraphElement {
   return {
     id: uuid.v4(),
@@ -72,7 +74,7 @@ function createFarmbot(
         dataSources: [],
       },
     ],
-    visible: true,
+    visible: deviceVisibility,
     position: {
       type: 'localization',
       stream: odometryStream,
@@ -84,85 +86,23 @@ function createFarmbot(
 }
 
 export function createScene(configuration: any) {
-  const devices = configuration?.devices?.map((device) =>
-    createFarmbot(device.name, device.deviceId, device.odometryStream)
-  );
+  const devices =
+    configuration?.devices?.map((device) =>
+      createFarmbot(
+        device.name,
+        device.deviceId,
+        device.odometryStream,
+        device.deviceVisibility
+      )
+    ) || [];
 
   const sg: SceneGraphElement[] = [
     createSatelliteLayer(
       configuration.baseStationLocationStream,
-      configuration.size
+      configuration.mapSize,
+      configuration.mapVisibility
     ),
     ...devices,
-
-    // createFarmbot('Farmbot 2', FARM_BOT_2_DEVICE_ID),
-    // createFarmbot('Farmbot 3', FARM_BOT_3_DEVICE_ID),
-    // {
-    //   id: uuid.v4(),
-    //   editing: false,
-    //   type: 'trail',
-    //   name: 'Farmbot',
-    //   deviceContext: FARM_BOT_3_DEVICE_ID,
-    //   children: [],
-    //   visible: true,
-    //   position: {
-    //     type: 'gps',
-    //     stream: 'farmbot.gps',
-    //     relativeToLatitude: 31.0119,
-    //     relativeToLongitude: -92.5499,
-    //   },
-    //   fieldValues: {},
-    //   data: {},
-    //   dataSources: [],
-    // },
-    /*
-    {
-      id: uuid.v4(),
-      editing: false,
-      type: 'video',
-      name: 'Video',
-      deviceContext: 'Abc',
-      children: [],
-      visible: true,
-      position: { type: 'manual', x: 0, y: 0, z: 0 },
-      scale: { x: 0.25, y: 0.25, z: 0.25 },
-      fieldValues: {
-        shape: {
-          type: 'text',
-          value: 'sphere',
-        },
-      },
-      data: {},
-      dataSources: [
-        {
-          id: uuid.v4(),
-          sourceType: 'realtime',
-          rosTopicName: '/joint_states',
-          rosTopicType: 'sensor_msgs/JointState',
-        },
-      ],
-    },
-    {
-      id: uuid.v4(),
-      editing: false,
-      type: 'video',
-      name: 'Video',
-      deviceContext: 'Abc',
-      children: [],
-      visible: true,
-      position: { type: 'hud', x: 0.2, y: 0.2 },
-      scale: { x: 0.25, y: 0.25, z: 0.25 },
-      fieldValues: {},
-      data: {},
-      dataSources: [
-        {
-          id: uuid.v4(),
-          sourceType: 'realtime',
-          rosTopicName: '/joint_states',
-          rosTopicType: 'sensor_msgs/JointState',
-        },
-      ],
-    }, */
     {
       id: uuid.v4(),
       editing: false,
